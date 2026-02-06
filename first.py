f41ef88641b1ac8d5d905e6f5b152417d43f303c
atifkhani397 <atif83837@gmail.com>
2026-02-05 22:47:02 -0800

    update

--- first.py
+++ first.py
@@ -1,3 +1,13 @@
 print( " hello world")
 print ( "My first commit")
 print (" hello everyone")
+x  = 10;
+print (x)
+# arithmetic operator 
+a = 3;
+b = 5;
+c = a+b;
+print (c)
+
+d = 5>4 
+print ( d)
