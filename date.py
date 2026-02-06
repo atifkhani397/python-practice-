ca553dba5b9b3948ce359b37d56eb49ba4100f3a
atifkhani397 <atif83837@gmail.com>
2026-02-06 06:01:02 -0800

    find index of word in string

---
+++ date.py
@@ -0,0 +1,8 @@
+name = "Harry is a good boy and   "
+# Return index of word is .... starting from which index .. 
+print ( name.find("is"))
+
+# String are immutable ....
+print ( name.replace( "  "," "))
+
+
